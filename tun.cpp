edf9de0aa5434f8568015b9461c2baad624c5b04
gnocuil <gnocuil@gmail.com>
2013-05-14 22:58:00 +0800

    TUN not working

--- tun.cpp
+++ tun.cpp
@@ -7,6 +7,7 @@
 #include <sys/ioctl.h>
 #include <iostream>
 #include <netinet/ip6.h>  
+#include <fcntl.h>
 
 #include "tun.h"
 #include "binding.h"
@@ -21,7 +22,7 @@ int tun_create(char *dev, int flags)
 	int err;
 
 	if ((tun_fd = open("/dev/net/tun", O_RDWR)) < 0) {
-		fprintf(stderr, "Error Creating TUN/TAP: %m\n", errno);
+		fprintf(stderr, "tun_create: Error Creating TUN/TAP: %m\n", errno);
 		return -1;
 	}
 
@@ -32,13 +33,15 @@ int tun_create(char *dev, int flags)
 		strncpy(ifr.ifr_name, dev, IFNAMSIZ);
 	}
 
-	if ((err = ioctl(tun_fd, TUNSETIFF, (void *)&ifr)) < 0) 
-	{
-		fprintf(stderr, "Error Setting tunnel name %s: %m\n", dev, errno);
+	if ((err = ioctl(tun_fd, TUNSETIFF, (void *)&ifr)) < 0) {
+		fprintf(stderr, "tun_create: Error Setting tunnel name %s: %m\n", dev, errno);
 		close(tun_fd);
 		return -1;
 	}
-
+	if (fcntl(tun_fd, F_SETFL, O_NONBLOCK) < 0) {
+		fprintf(stderr, "tun_create: Error Setting nonblock: %m\n", dev, errno);
+		return -1;
+	}
 	strcpy(dev, ifr.ifr_name);
 
 	return tun_fd;
@@ -52,6 +55,7 @@ int tun_send(char *packet, int len)
 
 int handle_tun()
 {
+	printf("tun_fd=%d\n", tun_fd);
 	uint16_t len = read(tun_fd, buf + 40, 2000);
 	uint32_t ip = *(uint32_t*)(buf + 40 + 16);
 	Binding* binding = find(ip, 0);
